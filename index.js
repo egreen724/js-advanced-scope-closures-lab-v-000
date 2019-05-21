a26b1dfaa6fc577a9fb40d267b70e0820cc1aca9
Erin Green <erin.y.green@gmail.com>
2019-05-21 17:49:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function inRange(startingPoint, endingPoint) {
       let result =  Number(endingPoint.slice(0, -2)) - Number(startingPoint.slice(0, -2))
-    if (result < blockRange) {
+    if (result <= blockRange) {
       return `within range by ${result}`
     }
     else {
